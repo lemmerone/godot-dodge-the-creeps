35ed347df714c4f8552871c1869e25b3fca048d6
Vadim Lemmer <lemmeronne@gmail.com>
2023-04-10 15:18:31 +0600

    feat: add HUD

--- scripts/main.gd
+++ scripts/main.gd
@@ -13,11 +13,16 @@ func _on_player_hit():
 func game_over():
 	$ScoreTimer.stop()
 	$MobTimer.stop()
+	$HUD.show_game_over()
 
 func new_game():
 	score = 0
 	$Player.start($StartPosition.position)
 	$StartTimer.start()
+	
+	$HUD.update_score(score)
+	$HUD.show_message("Get Ready")
+	get_tree().call_group("mobs", "queue_free")
 
 func _on_start_timer_timeout():
 	$MobTimer.start()
@@ -25,6 +30,7 @@ func _on_start_timer_timeout():
 
 func _on_score_timer_timeout():
 	score += 1
+	$HUD.update_score(score)
 
 func _on_mob_timer_timeout():
 	# создание новой сцены моба
